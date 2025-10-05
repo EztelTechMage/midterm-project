b7b418ef925df711d0d15bdc8e551f924d460b97
EztelTechMage <eztelwanderingmage@gmail.com>
2025-10-05 22:05:30 +0800

    fix: remove base path and add also vercel config for proper SPA routing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/midterm-project",
   server: {
     port: 3000,
     open: true,
