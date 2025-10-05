281787002ea15ddab65b82d2222374b8a3261da1
EztelTechMage <eztelwanderingmage@gmail.com>
2025-10-05 22:30:50 +0800

    i forgot to save the code changes on vercel.json and vite config before git add

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  // ADD this base path for your specific Vercel URL
   base: '/',
   server: {
     port: 3000,
